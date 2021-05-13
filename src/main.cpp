fb9ed2165516a0a21f53ee35a13046cc45503623
UnsignedByte <edlam1889@gmail.com>
2021-05-13 00:13:40 -0700

    video ouptut fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 * @Author: UnsignedByte
 * @Date:   2021-04-15 13:21:00
 * @Last Modified by:   UnsignedByte
-* @Last Modified time: 2021-05-12 11:36:58
+* @Last Modified time: 2021-05-13 00:13:27
 */
 
 #include <SFML/Graphics.hpp>
@@ -18,7 +18,7 @@ extern "C"
 	#include <libavcodec/avcodec.h>
 	#include <libavformat/avformat.h>
 	#include <libavutil/avutil.h>
-	#include <libavutil/avio.h>
+	#include <libavformat/avio.h>
 }
 #include "utils.hpp"
 
@@ -104,7 +104,16 @@ static void update() {
 	// std::cout << "theta: "<< theta << '\n';
 }
 
-static void encode(AVCodecContext* av_codec_context, AVFrame* frame, AVPacket* packet, FILE* file)
+static int write_frame(AVFormatContext* av_format_context, const AVRational& time_base, AVStream *av_stream, AVPacket *packet)
+{
+	/* rescale output packet timestamp values from codec to stream timebase */
+	av_packet_rescale_ts(packet, time_base, av_stream->time_base);
+	packet->stream_index = av_stream->index;
+	// printf("Stream Index: %d, PTS: %" PRId64 ", DTS: %" PRId64 ", pos: %" PRId64 ", duration: %" PRId64 "\n", packet->stream_index, packet->pts, packet->dts, packet->pos, packet->duration);
+	return av_interleaved_write_frame(av_format_context, packet);
+}
+
+static void encode(AVFormatContext* av_format_context, AVCodecContext* av_codec_context, AVStream* av_stream, AVFrame* frame, AVPacket* packet)
 {
 	int res;
 
@@ -114,20 +123,28 @@ static void encode(AVCodecContext* av_codec_context, AVFrame* frame, AVPacket* p
 		exit(1);
 	}
 
-	while(res >= 0) {
+	while(1) {
 		res = avcodec_receive_packet(av_codec_context, packet);
 
 		// all packets sent
 		if (res == AVERROR(EAGAIN) || res == AVERROR_EOF)
 		{
-			return;
+			// av_packet_free(&packet);
+			break;
 		} else if (res < 0) {
 			fprintf(stderr, "Error during encoding.\n");
 			exit(1);
 		}
 
-		// printf("Encoded frame %3" PRId64 " (size=%5d)\n", packet->pts, packet->size);
-		fwrite(packet->data, 1, packet->size, file);
+		// packet->pts = pts;
+		packet->duration = 1;
+		// printf("%ld\n", packet->duration);
+
+		res = write_frame(av_format_context, av_codec_context->time_base, av_stream, packet);
+		if (res < 0) {
+			fprintf(stderr, "Error muxing packet\n");
+			break;
+		}
 		av_packet_unref(packet);
 	}
 }
@@ -140,17 +157,28 @@ int main(int argc, char **argv)
 
 	asprintf(&filename, "output/%dPendulum%.2Lfkg%.3Lftheta_%dx%d_%dsecs%dfps_%.3fspeed%Lfdt.mp4", N, M, ANGLE, WIDTH, HEIGHT, duration, fps, speed, DT);
 
-	AVOutputFormat* av_output_format = av_guess_format(NULL, filename, NULL);
 	AVFormatContext* av_format_context;
 	AVCodecContext* av_codec_context;
 	AVCodec* av_codec;
-	FILE* file;
+	AVStream* av_stream;
   uint8_t endcode[] = { 0, 0, 1, 0xb7 };
 	int res; //stores error codes (if returned);
 
-	printf("Writing file with format %s (MIME type %s).\n", av_output_format->long_name, av_output_format->mime_type);
+	av_format_context = avformat_alloc_context();
+	if (!av_format_context) {
+		fprintf(stderr, "Could not allocate format context.\n");
+		exit(1);
+	}
 
-	av_codec = avcodec_find_encoder(av_output_format->video_codec);
+	res = avformat_alloc_output_context2(&av_format_context, av_guess_format(NULL, filename, NULL), NULL, filename);
+	if (res < 0){
+		printf("Setting output format failed with code %s\n", av_err2str(res));
+		exit(1);
+	}
+
+	printf("Writing file with format %s (MIME type %s).\n", av_format_context->oformat->long_name, av_format_context->oformat->mime_type);
+
+	av_codec = avcodec_find_encoder(av_format_context->oformat->video_codec);
 	if (!av_codec) {
 		fprintf(stderr, "Could not create video encoder.\n");
 		exit(1);
@@ -159,6 +187,12 @@ int main(int argc, char **argv)
 	av_codec_context = avcodec_alloc_context3(av_codec);
 	if (!av_codec_context) {
 		fprintf(stderr, "Could not create codec context.\n");
+		exit(1);
+	}
+
+	av_stream = avformat_new_stream(av_format_context, av_codec);
+	if (!av_stream) {
+		fprintf(stderr, "Could not create AVStream.\n");
 		exit(1);
 	}
 
@@ -174,15 +208,23 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	av_codec_context->bit_rate = 2000000;
 	av_codec_context->width = WIDTH;
 	av_codec_context->height = HEIGHT;
+	av_codec_context->bit_rate = av_codec_context->width*av_codec_context->height*4;
 	av_codec_context->time_base = (AVRational){1,fps};
 	av_codec_context->framerate = (AVRational){fps,1};
 
-	av_codec_context->gop_size = fps*0.5; //intra frame rate
+	av_codec_context->gop_size = fps; // intra frames
 	av_codec_context->max_b_frames = 1; //max number of B-frames between normal frames
-	av_codec_context->pix_fmt = AV_PIX_FMT_YUV444P; //color format
+	av_codec_context->pix_fmt = AV_PIX_FMT_YUV444P; //acolor format
+
+	frame->format = av_codec_context->pix_fmt;
+	frame->width = av_codec_context->width;
+	frame->height = av_codec_context->height;
+
+	av_stream->r_frame_rate = av_codec_context->framerate;
+	av_stream->avg_frame_rate = av_codec_context->framerate;
+	av_stream->time_base = av_codec_context->time_base;
 
 	//open codec
 	if (avcodec_open2(av_codec_context, av_codec, NULL) < 0) {
@@ -190,15 +232,28 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	file = fopen(filename, "wb");
-	if (!file) {
-		fprintf(stderr, "Could not open file: %s\n", filename);
-		exit(1);
+	// av_stream->codec = av_codec_context;
+
+	res = avcodec_parameters_from_context(av_stream->codecpar, av_codec_context);
+	if (res < 0) {
+		fprintf(stderr, "Failed to get avcodec params; Error code '%s'", av_err2str(res));
+    exit(1);
 	}
 
-	frame->format = av_codec_context->pix_fmt;
-	frame->width = av_codec_context->width;
-	frame->height = av_codec_context->height;
+	res = avio_open(&av_format_context->pb, filename, AVIO_FLAG_WRITE); // open output file
+	if (res < 0) {
+    fprintf(stderr, "Could not open output file '%s'", filename);
+    exit(1);
+	}
+
+	AVDictionary* av_opts = NULL;
+	av_dict_set(&av_opts, "movflags", "frag_keyframe+empty_moov+default_base_moof", 0);
+
+	res = avformat_write_header(av_format_context, &av_opts);
+	if (res < 0) {
+		fprintf(stderr, "Writing video header fails with code %s.\n", av_err2str(res));
+		exit(1);
+	}
 
 	res = av_frame_get_buffer(frame, 32);
 
@@ -210,8 +265,9 @@ int main(int argc, char **argv)
 
 		if ((i-lastFrame)*DT*fps-speed>=0){
 			lastFrame = i;
-			if (static_cast<int>(i*DT*fps/speed)%fps == 0)
-				printf("Writing frame %d\n", static_cast<int>(i*DT*fps/speed));
+			int frid = static_cast<int>(i*DT*fps/speed);
+			if (frid%fps == 0)
+				printf("Writing frame %d\n", frid);
 
 			fflush(stdout);
 			
@@ -239,19 +295,42 @@ int main(int argc, char **argv)
 				}
 			}
 
-			frame->pts = i;
+			// frame->pts = av_rescale_q(i, av_codec_context->time_base, av_stream->time_base);
+			frame->pts = frid;
 
 			//encode image
-			encode(av_codec_context, frame, packet, file);
+			encode(av_format_context, av_codec_context, av_stream, frame, packet);
 		}
 	}
 
 	//flush encoder
-	encode(av_codec_context, NULL, packet, file);
+	encode(av_format_context, av_codec_context, av_stream, NULL, packet);
 
 	//write file end code
-	fwrite(endcode, 1, sizeof(endcode), file);
-	fclose(file);
+	res = av_write_trailer(av_format_context);
+	if (res < 0) {
+		fprintf(stderr, "Writing video trailer fails with code %s.\n", av_err2str(res));
+		exit(1);
+	}
+
+	// printf(
+	// 	"\nCodec Parameters:\n"
+	// 	"Video size: %dx%d\n" 
+	// 	"Time base: %d/%d, Framerate: %d/%d\n"
+	// 	"\nStream Parameters:\n"
+	// 	"Time base: %d/%d\n"
+	// 	"Real Framerate: %d/%d\n"
+	// 	"Average Framerate: %d/%d\n"
+	// 	"Duration: %" PRId64 "\n"
+	// 	"Num frames: %" PRId64 "\n",
+	// 	av_codec_context->width, av_codec_context->height,
+	// 	av_codec_context->time_base.num, av_codec_context->time_base.den,
+	// 	av_codec_context->framerate.num, av_codec_context->framerate.den,
+	// 	av_stream->time_base.num, av_stream->time_base.den,
+	// 	av_stream->r_frame_rate.num, av_stream->r_frame_rate.den,
+	// 	av_stream->avg_frame_rate.num, av_stream->avg_frame_rate.den,
+	// 	av_stream->duration, av_stream->nb_frames
+	// 	);
 
 	avcodec_free_context(&av_codec_context);
 	av_frame_free(&frame);
